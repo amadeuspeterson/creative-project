73b2d21206a6261c0840c090f80ccd5b22ffb682
Greg Shipley <gregoryshipley@outlook.com>
2021-06-16 20:44:27 -0700

    Preliminary authentification

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,43 @@
 const express = require('express');
-const app = express();
+const bodyParser = require("body-parser");
 
-app.get('/', (req, res) => {
-  res.send("EA Sports");
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost:27017/profile', {
+  useNewUrlParser: true
 });
+
+const profileSchema = new mongoose.Schema({
+  name: String,
+  favNBA: String,
+  favMLB: String,
+  favNFL: String,
+});
+
+const Profile = mongoose.model('Profile', profileSchema);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const users = require("./users.js");
+app.use("/api/users", users.routes);
+
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
