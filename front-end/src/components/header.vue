101c3181ce1411e1b503b483f1909b33baad3ef2
gship11 <83378802+gship11@users.noreply.github.com>
2021-06-18 00:29:53 -0400

    Create header.vue

---
+++ front-end/src/components/header.vue
@@ -0,0 +1,24 @@
+<script>
+import axios from 'axios';
+export default {
+  name: 'MyPhotos',
+  data() {
+    return {}
+  },
+  computed: {
+    user() {
+      return this.$root.$data.user;
+    },
+  },
+  methods: {
+    async logout() {
+      try {
+        await axios.delete("/api/users");
+        this.$root.$data.user = null;
+      } catch (error) {
+        this.$root.$data.user = null;
+      }
+    },
+  }
+}
+</script>
